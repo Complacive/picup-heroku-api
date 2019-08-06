59fcecd8ec16172184af14776b38e78e78d3802c
Chun Lu <chun@Chun-Lu.local>
2019-08-06 16:40:23 -0400

    first

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
     console.log("This middleware function was executed");
     console.log(req.body);
